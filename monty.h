8e47387163d06e726e28db1b2ac71adb80a78e22
Meacha1 <meachattd@gmail.com>
2022-12-13 02:04:47 -0800

    commit message

---
+++ monty.h
@@ -0,0 +1,9 @@
+#ifndef MONTY_H
+#define MONTY_H
+#define MAX 100
+
+extern int top;
+extern int stack_arr[MAX];
+void push(int n);
+
+#endif
--- push.c
+++ push.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include "monty.h"
 
 void push(int n)
 {
 	top += 1;
-
+	stack_arr[top] = n;
 }
